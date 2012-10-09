8fe0014e19c0d232bc460652fb1649948b746af1
Alec Moskvin <alecm@gmx.com>
2012-10-09 17:23:59 -0400

    Update translations

--- translations/libqtxdg_eu.ts
+++ translations/libqtxdg_eu.ts
@@ -1,18 +1,18 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="es_VE" version="2.0">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="eu" version="2.0">
 <context>
     <name>XdgMenuReader</name>
     <message>
         <source>Menu file not defined.</source>
-        <translation>Archivo de menu no definido</translation>
+        <translation>Menu-fitxategia definitu gabe.</translation>
     </message>
     <message>
         <source>%1 not loading: %2</source>
-        <translation>%1 no esta cargado: %2</translation>
+        <translation>%1 ez da kargatu: %2</translation>
     </message>
     <message>
         <source>Parse error at line %1, column %2:
 %3</source>
-        <translation>Error al leer en linea %1, columna %2
+        <translation>Analisi-errorea %1. lerroko %2. zutabean:
 %3</translation>
     </message>
 </context>

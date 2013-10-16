lxqt/libqtxdg

commit 5a3027c93eb5f234537d0d58cc87df2fa80f5f3a

Use GNU installation directories
Use standard installation directories with GNUInstallDirs module which
also handle multilib paths.

Paths defined by GNUInstallDirs can also be customized passing arguments
to the CMake command line.

cmake/create_pkgconfig_file.cmake
@@ -14,8 +14,8 @@ macro (create_pkgconfig_file name desc requires include_rel_dir version)
     file(WRITE "${_pkgfname}"
             "# file generated by razor-qt cmake build\n"
             "prefix=${CMAKE_INSTALL_PREFIX}\n"
-            "libdir=\${prefix}/lib${LIB_SUFFIX}\n"
-            "includedir=\${prefix}/include\n"
+            "libdir=\${CMAKE_INSTALL_LIBDIR}\n"
+            "includedir=\${CMAKE_INSTALL_INCLUDEDIR}\n"
             "\n"
             "Name: ${name}\n"
             "Description: ${desc}\n"
@@ -26,5 +26,5 @@ macro (create_pkgconfig_file name desc requires include_rel_dir version)
             "\n"
         )
 
-    install(FILES ${_pkgfname} DESTINATION lib${LIB_SUFFIX}/pkgconfig)
+    install(FILES ${_pkgfname} DESTINATION ${CMAKE_INSTALL_LIBDIR}/pkgconfig)
 endmacro()

CMakeLists.txt
@@ -5,6 +5,9 @@ project(libqtxdg)
 option(USE_QT5 "Use Qt5. Defaults to Qt4" OFF)
 option(BUILD_TESTS "Builds tests" OFF)
 
+# Standard directories for installation
+include(GNUInstallDirs)
+
 # additional cmake files
 set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_SOURCE_DIR}/cmake)
 
@@ -152,7 +155,7 @@ configure_file(
 )
 
 # Instalable ...............................
-set(QTXDG_INCLUDE_DIR ${CMAKE_INSTALL_PREFIX}/include/${QTXDGX_FILE_NAME})
+set(QTXDG_INCLUDE_DIR ${CMAKE_INSTALL_INCLUDEDIR}/${QTXDGX_FILE_NAME})
 
 configure_file(
     ${CMAKE_CURRENT_SOURCE_DIR}/cmake/qtxdg-config.cmake.in
@@ -190,7 +193,7 @@ if (NOT CMAKE_BUILD_TYPE)
 endif (NOT CMAKE_BUILD_TYPE)
 
 
-set(APP_SHARE_DIR ${CMAKE_INSTALL_PREFIX}/share/lib${QTXDGX_FILE_NAME})
+set(APP_SHARE_DIR ${CMAKE_INSTALL_DATADIR}/lib${QTXDGX_FILE_NAME})
 add_definitions(-DTRANSLATIONS_DIR=\"${APP_SHARE_DIR}\")
 
 add_library(${QTXDGX_LIBRARY_NAME} SHARED
@@ -213,11 +216,11 @@ set_target_properties(${QTXDGX_LIBRARY_NAME} PROPERTIES
 include(create_portable_headers)
 create_portable_headers(libqtxdg_PORTABLE_HEADERS ${libqtxdg_PUBLIC_CLASSES})
 
-install(TARGETS ${QTXDGX_LIBRARY_NAME} DESTINATION lib${LIB_SUFFIX})
-install(FILES ${libqtxdg_PUBLIC_H_FILES} DESTINATION include/${QTXDGX_FILE_NAME})
-install(FILES ${CMAKE_CURRENT_BINARY_DIR}/${QTXDGX_FILE_NAME}-config.cmake DESTINATION share/cmake/${QTXDGX_FILE_NAME})
-install(FILES ${CMAKE_CURRENT_BINARY_DIR}/cmake/${QTXDGX_FILE_NAME}_use.cmake DESTINATION share/cmake/${QTXDGX_FILE_NAME})
-install(FILES ${libqtxdg_PORTABLE_HEADERS} DESTINATION include/${QTXDGX_FILE_NAME})
+install(TARGETS ${QTXDGX_LIBRARY_NAME} DESTINATION ${CMAKE_INSTALL_LIBDIR})
+install(FILES ${libqtxdg_PUBLIC_H_FILES} DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/${QTXDGX_FILE_NAME})
+install(FILES ${CMAKE_CURRENT_BINARY_DIR}/${QTXDGX_FILE_NAME}-config.cmake DESTINATION ${CMAKE_INSTALL_DATADIR}/cmake/${QTXDGX_FILE_NAME})
+install(FILES ${CMAKE_CURRENT_BINARY_DIR}/cmake/${QTXDGX_FILE_NAME}_use.cmake DESTINATION ${CMAKE_INSTALL_DATADIR}/cmake/${QTXDGX_FILE_NAME})
+install(FILES ${libqtxdg_PORTABLE_HEADERS} DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/${QTXDGX_FILE_NAME})
 
 include(create_pkgconfig_file)
 create_pkgconfig_file(${QTXDGX_LIBRARY_NAME}

commit 6d96d7a954b9682170cfb23fd82099f9b36a5ff1

Fixes bugs in create_pkgconfig_file cmake macro
Bugs introduced by commit 5a3027c93eb5f234537d0d58cc87df2fa80f5f3a
${prefix} was missing in ${libdir} and ${includedir} variables.
${CMAKE_INSTALL_LIBDIR} and ${CMAKE_INSTALL_INCLUDEDIR} cannot be escaped
in order to be substituted by their values.

Signed-off-by: Luís Pereira <luis.artur.pereira@gmail.com>

cmake/create_pkgconfig_file.cmake
@@ -14,8 +14,8 @@ macro (create_pkgconfig_file name desc requires include_rel_dir version)
     file(WRITE "${_pkgfname}"
             "# file generated by razor-qt cmake build\n"
             "prefix=${CMAKE_INSTALL_PREFIX}\n"
-            "libdir=\${CMAKE_INSTALL_LIBDIR}\n"
-            "includedir=\${CMAKE_INSTALL_INCLUDEDIR}\n"
+            "libdir=\${prefix}/${CMAKE_INSTALL_LIBDIR}\n"
+            "includedir=\${prefix}/${CMAKE_INSTALL_INCLUDEDIR}\n"
             "\n"
             "Name: ${name}\n"
             "Description: ${desc}\n"
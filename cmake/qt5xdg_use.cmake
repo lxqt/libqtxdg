28480fc47028a797c736747bf36eb0d0c1cf0969
Luís Pereira <luis.artur.pereira@gmail.com>
2014-08-26 11:57:28 -0700

    Build system maintenance
    * Makes ${QTXDG_LIBRARIES} also hold the dependencies libs
         ${QTXDG_LIBRARY} holds the qtxdg library itself. ${QTXDG_LIBRARIES}
         should hold qtxdg and all it's dependencies.
         See: http://www.cmake.org/Wiki/CMake:How_To_Find_Libraries
    
    * Qt4: Replaces ${QT_LIBRARIES} by ${QT_module_LIBRARY}
    * Qt5: Adds ${QTXDG_LIBRARY_DIRS} to the linker search paths

--- cmake/qt5xdg_use.cmake
+++ cmake/qt5xdg_use.cmake
@@ -1,11 +1,17 @@
 find_package(Qt5Widgets REQUIRED)
 find_package(Qt5Xml REQUIRED)
 
-include_directories(${Qt5Widgets_INCLUDE_DIRS} ${Qt5Xml_INCLUDE_DIRS})
 add_definitions(${Qt5Core_DEFINITIONS})
 set(CMAKE_CXX_FLAGS
     "${CMAKE_CXX_FLAGS} ${Qt5Widgets_EXECUTABLE_COMPILE_FLAGS}"
 )
 set(QTXDG_QT_LIBRARIES ${Qt5Widgets_LIBRARIES} ${Qt5Xml_LIBRARIES})
+set(QTXDG_LIBRARIES ${QTXDG_LIBRARIES} ${QTXDG_QT_LIBRARIES})
+set(QTXDG_INCLUDE_DIRS
+    ${QTXDG_INCLUDE_DIRS}
+    ${Qt5Widgets_INCLUDE_DIRS}
+    ${Qt5Xml_INCLUDE_DIRS}
+)
 
+link_directories(${QTXDG_LIBRARY_DIRS})
 include_directories(${QTXDG_INCLUDE_DIRS})

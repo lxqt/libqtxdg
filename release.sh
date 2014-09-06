cbc22cc1808c526fdb877b2a5bb158f868ed49be
Jerome Leclanche <jerome@leclan.ch>
2014-09-06 19:24:33 +0200

    Add a release script

---
+++ release.sh
@@ -0,0 +1,23 @@
+PROJECT="libqtxdg"
+version="$1"
+shift
+
+if [[ -z $version ]]; then
+	>&2 echo "USAGE: $0 <tag>"
+	exit 1
+fi
+
+mkdir -p "dist/$version"
+echo "Creating $PROJECT.tar.gz"
+git archive -9 --format tar.gz $version --prefix="$PROJECT/" > "dist/$version/$PROJECT.tar.gz"
+echo "Creating $PROJECT.tar.xz"
+git archive -9 --format tar.xz $version --prefix="$PROJECT/" > "dist/$version/$PROJECT.tar.xz"
+cd "dist/$version"
+
+sha1sum --tag *.tar.gz >> CHECKSUMS
+sha256sum --tag *.tar.gz >> CHECKSUMS
+
+cd ..
+echo "Uploading to lxqt.org..."
+
+scp -r "$version" "lxde.org:/var/www/lxqt/downloads/$PROJECT/"

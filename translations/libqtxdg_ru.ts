7631226c79adf8e71d1d01d51cf31ce5661489bb
Ilya87 <Ilya87@users.noreply.github.com>
2014-10-18 22:53:05 +0400

    Update Russian translation

--- translations/libqtxdg_ru.ts
+++ translations/libqtxdg_ru.ts
@@ -1,18 +1,24 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="ru" version="2.0">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru">
 <context>
     <name>XdgMenuReader</name>
     <message>
+        <location filename="../xdgmenureader.cpp" line="74"/>
         <source>Menu file not defined.</source>
-        <translation>Меню файл не определен.</translation>
+        <translation>Файл меню не определен.</translation>
     </message>
     <message>
+        <location filename="../xdgmenureader.cpp" line="91"/>
         <source>%1 not loading: %2</source>
         <translation>%1 не загружается: %2</translation>
     </message>
     <message>
+        <location filename="../xdgmenureader.cpp" line="103"/>
         <source>Parse error at line %1, column %2:
 %3</source>
-        <translation>ошибка парсера в строке %1, столбец%2⏎ %3</translation>
+        <translation>Ошибка парсера в строке %1, столбец%2:
+%3</translation>
     </message>
 </context>
 </TS>
--- translations/libqtxdg_ru_RU.ts
+++ translations/libqtxdg_ru_RU.ts
@@ -1,18 +1,23 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="ru_RU" version="2.0">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
 <context>
     <name>XdgMenuReader</name>
     <message>
+        <location filename="../xdgmenureader.cpp" line="74"/>
         <source>Menu file not defined.</source>
-        <translation>Не задан файл меню.</translation>
+        <translation>Файл меню не определен.</translation>
     </message>
     <message>
+        <location filename="../xdgmenureader.cpp" line="91"/>
         <source>%1 not loading: %2</source>
-        <translation>%1 не загружен: %2</translation>
+        <translation>%1 не загружается: %2</translation>
     </message>
     <message>
+        <location filename="../xdgmenureader.cpp" line="103"/>
         <source>Parse error at line %1, column %2:
 %3</source>
-        <translation>Ошибка разбора, сторка %1, столбец %2:
+        <translation>Ошибка парсера в строке %1, столбец%2:
 %3</translation>
     </message>
 </context>

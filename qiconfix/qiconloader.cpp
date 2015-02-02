548c282ec0146ed6ae2bbdad5518acb08a9c4258
Luís Pereira <luis.artur.pereira@gmail.com>
2015-02-02 11:45:59 -0800

    Use QPlatformTheme::SystemIconFallbackThemeName in static QString fallbackTheme()
    Instead of QPlatformTheme::SystemIconThemeName
    
    Port of commit
    https://qt.gitorious.org/qt/qtbase/commits/4dae1a685c3d78fa0601e8490117e9a62336c398

--- qiconfix/qiconloader.cpp
+++ qiconfix/qiconloader.cpp
@@ -75,7 +75,7 @@ static QString fallbackTheme()
 {
 #if 0
     if (const QPlatformTheme *theme = QGuiApplicationPrivate::platformTheme()) {
-        const QVariant themeHint = theme->themeHint(QPlatformTheme::SystemIconThemeName);
+        const QVariant themeHint = theme->themeHint(QPlatformTheme::SystemIconFallbackThemeName);
         if (themeHint.isValid())
             return themeHint.toString();
     }

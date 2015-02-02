ed3ed87b31fddfa9e28b8f4a7a2804cd961cb134
Luís Pereira <luis.artur.pereira@gmail.com>
2015-02-02 10:30:07 -0800

    QIconLoader: remove another unused variable
    Port of commit
    https://qt.gitorious.org/qt/qtbase/commits/8f0f7acb67a009c487fb3778e20a7d82d95f6661

--- qiconfix/qiconloader.cpp
+++ qiconfix/qiconloader.cpp
@@ -668,7 +668,6 @@ void QIconLoaderEngineFixed::virtual_hook(int id, void *data)
         {
             QIconEngine::AvailableSizesArgument &arg
                     = *reinterpret_cast<QIconEngine::AvailableSizesArgument*>(data);
-            const QList<QIconDirInfo> directoryKey = QIconLoader::instance()->theme().keyList();
             arg.sizes.clear();
 
             // Gets all sizes from the DirectoryInfo entries

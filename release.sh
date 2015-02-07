87fb74a1e7618ba199b977ee0d1ac341a3c13ee6
Jerome Leclanche <jerome@leclan.ch>
2015-02-07 18:23:22 +0100

    Update the release script to sign packages

--- release.sh
+++ release.sh
@@ -10,12 +10,14 @@ fi
 mkdir -p "dist/$version"
 echo "Creating $PROJECT.tar.gz"
 git archive -9 --format tar.gz $version --prefix="$PROJECT/" > "dist/$version/$PROJECT.tar.gz"
+gpg --armor --detach-sign "dist/$version/$PROJECT.tar.gz"
 echo "Creating $PROJECT.tar.xz"
 git archive -9 --format tar.xz $version --prefix="$PROJECT/" > "dist/$version/$PROJECT.tar.xz"
+gpg --armor --detach-sign "dist/$version/$PROJECT.tar.xz"
 cd "dist/$version"
 
-sha1sum --tag *.tar.gz >> CHECKSUMS
-sha256sum --tag *.tar.gz >> CHECKSUMS
+sha1sum --tag *.tar.gz *.tar.xz >> CHECKSUMS
+sha256sum --tag *.tar.gz *.tar.xz >> CHECKSUMS
 
 cd ..
 echo "Uploading to lxqt.org..."

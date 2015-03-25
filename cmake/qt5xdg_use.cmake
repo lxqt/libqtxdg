d3ba6ef8b248f5d80ce5b3c0c0cd7047eb8452d5
Luís Pereira <luis.artur.pereira@gmail.com>
2015-03-25 07:27:59 -0700

    Add QtDBus/Qt5DBus dependency
    We depend on it for the DBusActivation support.

--- cmake/qt5xdg_use.cmake
+++ cmake/qt5xdg_use.cmake
@@ -1,5 +1,6 @@
 find_package(Qt5Widgets REQUIRED)
 find_package(Qt5Xml REQUIRED)
+find_package(Qt5DBus REQUIRED)
 
 if (QTXDG_QTMIMETYPES)
     add_definitions("-DQT_MIMETYPES")
@@ -9,12 +10,13 @@ add_definitions(${Qt5Core_DEFINITIONS})
 set(CMAKE_CXX_FLAGS
     "${CMAKE_CXX_FLAGS} ${Qt5Widgets_EXECUTABLE_COMPILE_FLAGS}"
 )
-set(QTXDG_QT_LIBRARIES ${Qt5Widgets_LIBRARIES} ${Qt5Xml_LIBRARIES})
+set(QTXDG_QT_LIBRARIES ${Qt5Widgets_LIBRARIES} ${Qt5Xml_LIBRARIES} ${Qt5DBus_LIBRARIES})
 set(QTXDG_LIBRARIES ${QTXDG_LIBRARIES} ${QTXDG_QT_LIBRARIES})
 set(QTXDG_INCLUDE_DIRS
     "${QTXDG_INCLUDE_DIRS}"
     "${Qt5Widgets_INCLUDE_DIRS}"
     "${Qt5Xml_INCLUDE_DIRS}"
+    "${Qt5DBus_INCLUDE_DIRS}"
 )
 
 link_directories("${QTXDG_LIBRARY_DIRS}")

44524bdb1060afd18903edd25afd12b69114363b
Luís Pereira <luis.artur.pereira@gmail.com>
2016-12-22 19:36:01 +0000

    Optimize QIconLoader::findIconHelper()
    Profiling QIconLoader::findIconHelper() shows that a significant portion of CPU time is being spent in
    QDir::exists(), which creates a new QFileInfo object for the sole purpose of determining whether the passed-in
    file path is relative or absolute, and then calls QFile::exists(). In this context, we can just as easily
    generate the absolute path and call QFile::exists() directly, avoiding the creation of extra QDir and QFileInfo
    objects.
    
    Change-Id: Ib0b4568b6c16d423eb6c1b15158e44ff141e6175
    Task-number: QTBUG-46767
    Reviewed-by: Gunnar Sletta <gunnar@sletta.org>
    
    Port of:
    https://code.qt.io/cgit/qt/qtbase.git/commit/src/gui/image/qiconloader.cpp?id=36aaf851ff2814e9e5c024e21b866c403137ff26
    
    Note: Didn't port to our enhancements to the icon loader. They are not in
    a loop, so the performance improvements are insignificant.

--- xdgiconloader/xdgiconloader.cpp
+++ xdgiconloader/xdgiconloader.cpp
@@ -357,25 +357,30 @@ QThemeIconInfo XdgIconLoader::findIconHelper(const QString &themeName,
             QString contentDir = contentDirs.at(i) + QLatin1Char('/');
             for (int j = 0; j < subDirs.size() ; ++j) {
                 const QIconDirInfo &dirInfo = subDirs.at(j);
-                QString subdir = dirInfo.path;
-                QDir currentDir(contentDir + subdir);
-                if (currentDir.exists(pngIconName)) {
+                const QString subDir = contentDir + dirInfo.path + QLatin1Char('/');
+                const QString pngPath = subDir + pngIconName;
+                if (QFile::exists(pngPath)) {
                     PixmapEntry *iconEntry = new PixmapEntry;
                     iconEntry->dir = dirInfo;
-                    iconEntry->filename = currentDir.filePath(pngIconName);
+                    iconEntry->filename = pngPath;
                     // Notice we ensure that pixmap entries always come before
                     // scalable to preserve search order afterwards
                     info.entries.prepend(iconEntry);
-                } else if (gSupportsSvg &&
-                    currentDir.exists(svgIconName)) {
-                    ScalableEntry *iconEntry = new ScalableEntry;
-                    iconEntry->dir = dirInfo;
-                    iconEntry->filename = currentDir.filePath(svgIconName);
-                    info.entries.append(iconEntry);
-                } else if(currentDir.exists(iconName + xpmext)) {
+                } else {
+                    const QString svgPath = subDir + svgIconName;
+                    if (gSupportsSvg && QFile::exists(svgPath)) {
+                        ScalableEntry *iconEntry = new ScalableEntry;
+                        iconEntry->dir = dirInfo;
+                        iconEntry->filename = svgPath;
+                        info.entries.append(iconEntry);
+                        break;
+                    }
+                }
+                const QString xpmPath = subDir + xpmIconName;
+                if (QFile::exists(xpmPath)) {
                     PixmapEntry *iconEntry = new PixmapEntry;
                     iconEntry->dir = dirInfo;
-                    iconEntry->filename = currentDir.filePath(iconName + xpmext);
+                    iconEntry->filename = xpmPath;
                     // Notice we ensure that pixmap entries always come before
                     // scalable to preserve search order afterwards
                     info.entries.append(iconEntry);

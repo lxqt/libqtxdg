2eb520206b3b443495cebe77be8819007efba8d5
Luís Pereira <luis.artur.pereira@gmail.com>
2017-12-22 18:11:28 +0000

    Don't allow Q_FOREACH to be used
    Enforce the fact that we are Q_FOREACH-free.

--- cmake/compiler_settings.cmake
+++ cmake/compiler_settings.cmake
@@ -118,6 +118,7 @@ add_definitions(
     -DQT_NO_CAST_TO_ASCII
     -DQT_NO_URL_CAST_FROM_STRING
     -DQT_NO_CAST_FROM_BYTEARRAY
+    -DQT_NO_FOREACH
 )
 
 #-----------------------------------------------------------------------------
